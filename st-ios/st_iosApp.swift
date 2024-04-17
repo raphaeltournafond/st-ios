1b910ad2cc41f5847b6da13fbb827ad5d5b34da7
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-17 09:50:24 +0200

    Initial Commit

---
+++ st-ios/st_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  st_iosApp.swift
+//  st-ios
+//
+//  Created by Raphaël Tournafond on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct st_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
