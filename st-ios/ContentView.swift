1cb646bb08abc13b4be6548f8b5120e958abdd14
Raphaël Tournafond <32420506+raphaeltournafond@users.noreply.github.com>
2024-04-17 12:15:10 +0200

    feat: add base UI

--- st-ios/ContentView.swift
+++ st-ios/ContentView.swift
@@ -6,16 +6,71 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    @State private var peripherals: [CBPeripheral] = []
+    @State private var isScanning = false
+    @State private var timer: Timer?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Smart Tracker")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+            Button(action: {
+                self.toggleScan()
+            }) {
+                Text(isScanning ? "Stop Scanning" : "Start Scanning")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+            if isScanning {
+                ProgressView("Scanning...")
+                    .padding()
+            }
+            
+            Text("BLE peripherals").multilineTextAlignment(.trailing)
+
+            List(peripherals, id: \.self) { peripheral in
+                Text(peripheral.name ?? "Unknown")
+            }
         }
-        .padding()
+        .onAppear {
+            self.startScanning()
+            self.timer = Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { _ in
+                self.stopScanning()
+            }
+        }
+        .onDisappear {
+            self.timer?.invalidate()
+            self.timer = nil
+        }
+    }
+
+    func startScanning() {
+        isScanning = true
+    }
+
+    func stopScanning() {
+        isScanning = false
+    }
+
+    func toggleScan() {
+        if isScanning {
+            stopScanning()
+        } else {
+            startScanning()
+            self.timer?.invalidate()
+            self.timer = Timer.scheduledTimer(withTimeInterval: 10, repeats: false) { _ in
+                self.stopScanning()
+            }
+        }
     }
 }
 
